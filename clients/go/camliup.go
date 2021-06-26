e1ff2158ff263a67d12114a9fb397293379d8de3
manunio <max.manu.nair@gmail.com>
2021-06-26 22:00:28 +0530

    preupload request in upload client

--- clients/go/camliup.go
+++ clients/go/camliup.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha1"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
+	"strings"
 )
 
 var flagFile = flag.String("file", "", "file to upload")
@@ -25,9 +29,53 @@ func NewAgent(server string) *Agent {
 	return &Agent{server}
 }
 
-func (a *Agent) Upload(handle *UploadHandler) {
+func (a *Agent) Upload(h *UploadHandler) {
 	// TODO:
-	fmt.Println("Need to upload:", handle)
+	url := fmt.Sprintf("%s/camli/preupload", a.server)
+	fmt.Println("Need to upload: ", h, "to", url)
+
+	e := func(msg string, e error) {
+		_, _ = fmt.Fprintf(os.Stderr, "%s on %v: %v\n", msg, h.blobRef, e)
+		return
+	}
+
+	resp, err := http.Post(
+		url,
+		"application/x-www-form-urlencoded",
+		strings.NewReader("camliversion=1&blob1="+h.blobRef))
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Upload error for %v: %v\n",
+			h.blobRef, err)
+	}
+	fmt.Println("Got response:", resp)
+	buf := new(bytes.Buffer)
+	_, _ = io.Copy(buf, resp.Body)
+	_ = resp.Body.Close()
+
+	pur := make(map[string]interface{})
+	jerr := json.Unmarshal(buf.Bytes(), &pur)
+	if jerr != nil {
+		e("preupload parse error", jerr)
+		return
+	}
+	uploadUrl, ok := pur["uploadUrl"].(string)
+	if uploadUrl == "" {
+		e("no uploadUrl in preupload response", nil)
+		return
+	}
+	alreadyHave, ok := pur["alreadyHave"].([]interface{})
+	if !(ok) {
+		e("no alreadyHave array in preupload response", nil)
+	}
+
+	for _, haveObj := range alreadyHave {
+		haveObj := haveObj.(map[string]interface{})
+		if haveObj["blobRef"].(string) == h.blobRef {
+			fmt.Println("already have it!")
+			return
+		}
+	}
+	fmt.Println("preupload done:", pur, alreadyHave)
 }
 
 func (a *Agent) Wait() int {
@@ -51,18 +99,24 @@ func uploadFile(agent *Agent, filename string) error {
 	if err != nil {
 		return err
 	}
-	blobname, err := blobName(file)
+	blobRef, err := blobName(file)
 	if err != nil {
 		return err
 	}
-	fmt.Println("blob is: ", blobname)
-	handle := &UploadHandler{blobname, file}
+	fmt.Println("blob is: ", blobRef)
+	handle := &UploadHandler{blobRef, file}
 	agent.Upload(handle)
 	return nil
 }
 
 func main() {
 	flag.Parse()
+
+	// remove trailing slash if provided
+	if strings.HasSuffix(*flagServer, "/") {
+		*flagServer = (*flagServer)[0 : len(*flagServer)-1]
+	}
+
 	agent := NewAgent(*flagServer)
 	if *flagFile != "" {
 		if err := uploadFile(agent, *flagFile); err != nil {
@@ -71,6 +125,7 @@ func main() {
 		}
 
 	}
+
 	stats := agent.Wait()
 	fmt.Println("Done uploading stats: ", stats)
 }
